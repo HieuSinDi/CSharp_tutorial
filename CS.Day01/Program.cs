cd33fd024e29c647fb35472245b3615410fa9c05
HieuSinDi <quanghieu10hd@gmail.com>
2021-11-07 22:29:50 +0700

    CS.Day01

---
+++ CS.Day01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CS.Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello cc");
+        }
+    }
+}
